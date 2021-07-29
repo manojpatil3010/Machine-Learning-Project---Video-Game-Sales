43e1ca94e3498d6dc43a02adb38d80b45c5859aa
manojpatil3010 <86981479+manojpatil3010@users.noreply.github.com>
2021-07-29 18:43:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 sns.set_style("darkgrid")
 
